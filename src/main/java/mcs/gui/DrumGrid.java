019e90738b8d1061d7023ae9867f82be17cc6c55
Mathieu Bordas <mat.bordas@gmail.com>
2019-04-27 20:55:10 +0200

    fix: DrumGrid / toPattern()

--- src/main/java/mcs/gui/DrumGrid.java
+++ src/main/java/mcs/gui/DrumGrid.java
@@ -28,212 +28,199 @@ import java.util.Map;
 
 public class DrumGrid extends JComponent {
 
-	public static final int CELL_WIDTH_px = 200;
-	public static final int CELL_HEIGHT_px = 200;
-	public static final int MARGIN_px = 100;
-	public static final int CELL_PADDING_px = 10;
+    public static final int CELL_WIDTH_px = 200;
+    public static final int CELL_HEIGHT_px = 200;
+    public static final int MARGIN_px = 100;
+    public static final int CELL_PADDING_px = 10;
 
-	static Color m_backgroundColor = Color.darkGray;
-	static Color m_emptyCellColor = Color.lightGray;
-	static Color m_filledCellColor = Color.orange;
+    static Color m_backgroundColor = Color.darkGray;
+    static Color m_emptyCellColor = Color.lightGray;
+    static Color m_filledCellColor = Color.orange;
 
-	// Graphic
-	private Image m_image;
-	private Graphics2D m_graphics2D;
+    // Graphic
+    private Image m_image;
+    private Graphics2D m_graphics2D;
 
-	// Pattern
-	Time.TimeSignature m_timeSignature;
-	Map<String, Integer> m_keyMapping;
-	int[][] m_velocityMatrix;
-	int m_currentVelocity = Note.DEFAULT_VELOCITY;
+    // Pattern
+    Time.TimeSignature m_timeSignature;
+    Map<String, Integer> m_keyMapping;
+    int[][] m_velocityMatrix;
+    int m_currentVelocity = Note.DEFAULT_VELOCITY;
 
-	// Edition
-	enum EditionMode {WRITE, ERASE, MOVE}
+    // Edition
+    enum EditionMode {WRITE, ERASE, MOVE}
 
-	int m_ticksPerBeat = 2;
-	EditionMode m_mode = EditionMode.MOVE;
+    int m_ticksPerBeat = 2;
+    EditionMode m_mode = EditionMode.MOVE;
 
-	public DrumGrid(Time.TimeSignature timeSignature, int bars, Map<String, Integer> keyMapping) {
-		m_timeSignature = timeSignature;
-		m_keyMapping = keyMapping;
+    public DrumGrid(Time.TimeSignature timeSignature, int bars, Map<String, Integer> keyMapping) {
+        m_timeSignature = timeSignature;
+        m_keyMapping = keyMapping;
 
-		// Total number of raws in grid (x-axis)
-		int columns = bars * timeSignature.getBeatsInBar() * m_ticksPerBeat;
-		int rows = m_keyMapping.size();
-		// Each available key is a line (y-axis)
-		m_velocityMatrix = new int[columns][rows];
-		for(int x = 0; x < columns; x++) {
-			m_velocityMatrix[x] = new int[rows];
-			for(int y = 0; y < rows; y++) {
-				m_velocityMatrix[x][y] = 0;
-			}
-		}
+        // Total number of raws in grid (x-axis)
+        int columns = bars * timeSignature.getBeatsInBar() * m_ticksPerBeat;
+        int rows = m_keyMapping.size();
+        // Each available key is a line (y-axis)
+        m_velocityMatrix = new int[columns][rows];
+        for (int x = 0; x < columns; x++) {
+            m_velocityMatrix[x] = new int[rows];
+            for (int y = 0; y < rows; y++) {
+                m_velocityMatrix[x][y] = 0;
+            }
+        }
 
-		setDoubleBuffered(false);
+        setDoubleBuffered(false);
 
-		addMouseListener(new MouseListener() {
-			@Override
-			public void mouseClicked(MouseEvent mouseEvent) {
-			}
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+            }
 
-			@Override
-			public void mousePressed(MouseEvent event) {
-				if(event.getButton() == 1) {
-					m_mode = EditionMode.WRITE;
-				} else if(event.getButton() == 3) {
-					m_mode = EditionMode.ERASE;
-				}
+            @Override
+            public void mousePressed(MouseEvent event) {
+                if (event.getButton() == 1) {
+                    m_mode = EditionMode.WRITE;
+                } else if (event.getButton() == 3) {
+                    m_mode = EditionMode.ERASE;
+                }
 
-				System.out.println("edition mode: " + m_mode);
+                System.out.println("edition mode: " + m_mode);
 
-				onMouseAction(event);
-			}
+                onMouseAction(event);
+            }
 
-			@Override
-			public void mouseReleased(MouseEvent mouseEvent) {
-				m_mode = EditionMode.MOVE;
-			}
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+                m_mode = EditionMode.MOVE;
+            }
 
-			@Override
-			public void mouseEntered(MouseEvent mouseEvent) {
-			}
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+            }
 
-			@Override
-			public void mouseExited(MouseEvent mouseEvent) {
-			}
-		});
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+            }
+        });
 
-		addMouseMotionListener(new MouseMotionAdapter() {
-			public void mouseDragged(MouseEvent event) {
-				onMouseAction(event);
-			}
-		});
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent event) {
+                onMouseAction(event);
+            }
+        });
 
-		int width_px = 2 * MARGIN_px + getMatrixWidth() * CELL_WIDTH_px;
-		int height_px = 2 * MARGIN_px + getMatrixHeight() * CELL_HEIGHT_px;
-		System.out.println("Drum grid dimension: " + width_px + " x " + height_px);
-		setSize(width_px, height_px);
-		setPreferredSize(new Dimension(width_px, height_px));
-	}
+        int width_px = 2 * MARGIN_px + getMatrixWidth() * CELL_WIDTH_px;
+        int height_px = 2 * MARGIN_px + getMatrixHeight() * CELL_HEIGHT_px;
+        System.out.println("Drum grid dimension: " + width_px + " x " + height_px);
+        setSize(width_px, height_px);
+        setPreferredSize(new Dimension(width_px, height_px));
+    }
 
-	int getMatrixWidth() {
-		return m_velocityMatrix.length;
-	}
+    int getMatrixWidth() {
+        return m_velocityMatrix.length;
+    }
 
-	int getMatrixHeight() {
-		return m_velocityMatrix[0].length;
-	}
+    int getMatrixHeight() {
+        return m_velocityMatrix[0].length;
+    }
 
-	boolean isInGrid(int columns, int row) {
-		if(columns < 0 || columns >= getMatrixWidth()) {
-			return false;
-		}
-		if(row < 0 || row >= getMatrixHeight()) {
-			return false;
-		}
-		return true;
-	}
+    boolean isInGrid(int columns, int row) {
+        if (columns < 0 || columns >= getMatrixWidth()) {
+            return false;
+        }
+        if (row < 0 || row >= getMatrixHeight()) {
+            return false;
+        }
+        return true;
+    }
 
-	void write(int column, int row) {
-		m_velocityMatrix[column][row] = m_currentVelocity;
-	}
+    void write(int column, int row) {
+        m_velocityMatrix[column][row] = m_currentVelocity;
+    }
 
-	void erase(int column, int row) {
-		m_velocityMatrix[column][row] = 0;
-	}
+    void erase(int column, int row) {
+        m_velocityMatrix[column][row] = 0;
+    }
 
-	void onMouseAction(MouseEvent event) {
-		int column = pixel2column(event.getX());
-		int row = pixel2row(event.getY());
+    void onMouseAction(MouseEvent event) {
+        int column = pixel2column(event.getX());
+        int row = pixel2row(event.getY());
 
-		if(isInGrid(column, row)) {
-			if(m_graphics2D != null) {
-				if(m_mode == EditionMode.MOVE) {
-					return;
-				}
+        if (isInGrid(column, row)) {
+            if (m_graphics2D != null) {
+                if (m_mode == EditionMode.MOVE) {
+                    return;
+                }
 
-				if(m_mode == EditionMode.WRITE) {
-					write(column, row);
-					m_graphics2D.setPaint(m_filledCellColor);
-				} else if(m_mode == EditionMode.ERASE) {
-					erase(column, row);
-					m_graphics2D.setPaint(m_emptyCellColor);
-				}
+                if (m_mode == EditionMode.WRITE) {
+                    write(column, row);
+                    m_graphics2D.setPaint(m_filledCellColor);
+                } else if (m_mode == EditionMode.ERASE) {
+                    erase(column, row);
+                    m_graphics2D.setPaint(m_emptyCellColor);
+                }
 
-				m_graphics2D.fillRect(MARGIN_px + column * CELL_WIDTH_px + CELL_PADDING_px,
-						MARGIN_px + row * CELL_HEIGHT_px + CELL_PADDING_px, CELL_WIDTH_px - 2 * CELL_PADDING_px,
-						CELL_HEIGHT_px - 2 * CELL_PADDING_px);
-				repaint();
-			}
-		}
-	}
+                m_graphics2D.fillRect(MARGIN_px + column * CELL_WIDTH_px + CELL_PADDING_px,
+                        MARGIN_px + row * CELL_HEIGHT_px + CELL_PADDING_px, CELL_WIDTH_px - 2 * CELL_PADDING_px,
+                        CELL_HEIGHT_px - 2 * CELL_PADDING_px);
+                repaint();
+            }
+        }
+    }
 
-	protected void paintComponent(Graphics g) {
-		if(m_image == null) {
-			m_image = createImage(getSize().width, getSize().height);
-			m_graphics2D = (Graphics2D) m_image.getGraphics();
-			// Enable antialiasing
-			m_graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    protected void paintComponent(Graphics g) {
+        if (m_image == null) {
+            m_image = createImage(getSize().width, getSize().height);
+            m_graphics2D = (Graphics2D) m_image.getGraphics();
+            // Enable antialiasing
+            m_graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-			clear();
-		}
+            clear();
+        }
 
-		g.drawImage(m_image, 0, 0, null);
-	}
+        g.drawImage(m_image, 0, 0, null);
+    }
 
-	public void clear() {
-		// Clearing velocity matrix
-		for(int x = 0; x < getMatrixWidth(); x++) {
-			for(int y = 0; y < getMatrixHeight(); y++) {
-				m_velocityMatrix[x][y] = 0;
-			}
-		}
+    public void clear() {
+        // Clearing velocity matrix
+        for (int x = 0; x < getMatrixWidth(); x++) {
+            for (int y = 0; y < getMatrixHeight(); y++) {
+                m_velocityMatrix[x][y] = 0;
+            }
+        }
 
-		// Clearing display
-		m_graphics2D.setPaint(m_backgroundColor);
-		m_graphics2D.fillRect(0, 0, getSize().width, getSize().height);
-		m_graphics2D.setColor(m_emptyCellColor);
-		m_graphics2D.fillRect(MARGIN_px, MARGIN_px, getMatrixWidth() * CELL_WIDTH_px, getMatrixHeight() * CELL_HEIGHT_px);
-		repaint();
-	}
+        // Clearing display
+        m_graphics2D.setPaint(m_backgroundColor);
+        m_graphics2D.fillRect(0, 0, getSize().width, getSize().height);
+        m_graphics2D.setColor(m_emptyCellColor);
+        m_graphics2D.fillRect(MARGIN_px, MARGIN_px, getMatrixWidth() * CELL_WIDTH_px, getMatrixHeight() * CELL_HEIGHT_px);
+        repaint();
+    }
 
-	public Pattern toPattern(int channel) {
-		Pattern result = new Pattern(m_timeSignature, m_ticksPerBeat);
+    public Pattern toPattern(int channel) {
+        Pattern result = new Pattern(m_timeSignature, m_ticksPerBeat);
 
-		int row = 0;
-		for(int key : m_keyMapping.values()) {
-			int _velocity = 0; // previous velocity
-			int tickStart = -1;
+        int row = 0;
+        for (int key : m_keyMapping.values()) {
+            for (int tick = 0; tick < getMatrixWidth(); tick++) {
+                int velocity = m_velocityMatrix[tick][row];
 
-			for(int tick = 0; tick < getMatrixWidth(); tick++) {
-				int velocity = m_velocityMatrix[tick][row];
-				if(velocity != _velocity) {
-					// Velocity changes from previous cell
-					if(velocity > 0) {
-						tickStart = tick;
-					} else {
-						result.add(channel, key, _velocity, tickStart, tick);
-					}
-				}
+                if (velocity > 0) {
+                    result.add(channel, key, velocity, tick, tick + 1);
+                }
+            }
 
-				_velocity = velocity;
-			}
+            row++;
+        }
 
-			if(_velocity > 0) {
-				result.add(channel, key, _velocity, tickStart, getMatrixWidth());
-			}
+        return result;
+    }
 
-			row++;
-		}
+    static int pixel2column(int mouseX_px) {
+        return (mouseX_px - MARGIN_px) / CELL_WIDTH_px;
+    }
 
-		return result;
-	}
-
-	static int pixel2column(int mouseX_px) {
-		return (mouseX_px - MARGIN_px) / CELL_WIDTH_px;
-	}
-
-	static int pixel2row(int mouseY_px) {
-		return (mouseY_px - MARGIN_px) / CELL_HEIGHT_px;
-	}
+    static int pixel2row(int mouseY_px) {
+        return (mouseY_px - MARGIN_px) / CELL_HEIGHT_px;
+    }
 }
